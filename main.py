6ed749be64f5b78842f5cae39a924720c56d0fc8
mhamdikhilaahmed-coder <mhamdi.khila.ahmed@iesllavaneres.org>
2025-11-06 00:50:51 +0100

    main.py

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 # main.py (simplificado para ejemplo)
 print("Nuvix FullPack - todos los bots listos con Pycord slash+prefijo")
+print("Nuvix FullPack — todos los bots listos con Pycord slash+prefijo")
+
+# Evitar que Render cierre la app:
+import time
+while True:
+    time.sleep(3600)
